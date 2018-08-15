a9415e7e4ce802dcd427569a5b9ec099d951605b
QuanTM9 <quantm9@fsoft.com.vn>
2018-08-15 09:29:40 +0700

    init ngan luong

---
+++ app/src/main/java/com/yd/yourdoctorandroid/ui/BaseActivity.java
@@ -0,0 +1,31 @@
+package com.yd.yourdoctorandroid.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+import vn.demonganluong.R;
+
+/**
+ * Created by DucChinh on 6/13/2016.
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/Roboto-Regular.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        );
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
---
+++ app/src/main/java/com/yd/yourdoctorandroid/utils/Constant.java
@@ -0,0 +1,17 @@
+package com.yd.yourdoctorandroid.utils;
+
+/**
+ * Created by DucChinh on 6/13/2016.
+ */
+public class Constant {
+
+    public static final String MAIN_URL = "https://www.nganluong.vn/mobile_checkout_api_post.php";
+    public static final String RETURN_URL = "https://www.nganluong.vn/nganluong/homeDeveloper/DeveloperBank.html";
+    public static final String CANCEL_URL = "https://www.nganluong.vn/nganluong/homeDeveloper/DeveloperBank.html";
+    public static final String NOTIFY_URL = "https://www.nganluong.vn/nganluong/homeDeveloper/DeveloperBank.html";
+
+    public static final int TIMEOUT = 30000;
+
+    public static final String MERCHANT_ID = "45060";
+    public static final String MERCHANT_PASSWORD = "9fff39a446b7af9d5757b3c20cf3a9c1";
+}
